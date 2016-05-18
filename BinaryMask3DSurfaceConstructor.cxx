a92ec3f1c832e99e913b2cf60ff39081ef657bf1
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2016-05-18 09:15:02 +0300

    update binaryMask3DSurfaceConstructor

--- BinaryMask3DSurfaceConstructor.cxx
+++ BinaryMask3DSurfaceConstructor.cxx
@@ -11,6 +11,9 @@
 #include <vtkSmoothPolyDataFilter.h>
 #include <vtkPolyDataNormals.h>
 #include <itkImageToVTKImageFilter.h>
+#include <itkZeroCrossingImageFilter.h>
+#include <itkSignedMaurerDistanceMapImageFilter.h>
+#include <itkAddImageFilter.h>
 
 #include "utils/io.h"
 #include "utils/itkCommandLineArgumentParser.h"
@@ -26,44 +29,61 @@ int main(int argc, char** argv) {
 
   parser->SetCommandLineArguments(argc, argv);
 
-  std::string imageFile;
-  parser->GetCommandLineArgument("-image", imageFile);
+  std::string labelFile;
+  parser->GetCommandLineArgument("-label", labelFile);
 
-  std::string outputFile;
-  parser->GetCommandLineArgument("-output", outputFile);
+  std::string outputSurfaceFile;
+  parser->GetCommandLineArgument("-surface", outputSurfaceFile);
 
-  float spacing = 2;
+  std::string outputPotentialFile;
+  parser->GetCommandLineArgument("-potential", outputPotentialFile);
+
+  float spacing = 1;
   parser->GetCommandLineArgument("-spacing", spacing);
 
   float sigma = 1;
   parser->GetCommandLineArgument("-sigma", sigma);
 
-  float relaxationFactor = 0.2;
-  parser->GetCommandLineArgument("-relaxation", relaxationFactor);
+  float relaxation = 0.2;
+  parser->GetCommandLineArgument("-relaxation", relaxation);
 
-  int numberOfIterations = 10;
-  parser->GetCommandLineArgument("-iteration", numberOfIterations);
+  int iterations = 10;
+  parser->GetCommandLineArgument("-iteration", iterations);
 
+  std::cout << std::endl;
   std::cout << "input parameters" << std::endl;
   std::cout << "    spacing " << spacing << std::endl;
   std::cout << "      sigma " << sigma << std::endl;
-  std::cout << " relaxation " << relaxationFactor << std::endl;
-  std::cout << " iterations " << numberOfIterations << std::endl;
+  std::cout << " relaxation " << relaxation << std::endl;
+  std::cout << " iterations " << iterations << std::endl;
   std::cout << std::endl;
 
   //----------------------------------------------------------------------------
   // read image
 
-  BinaryImageType::Pointer image = BinaryImageType::New();
-  if (!readImage<BinaryImageType>(image, imageFile)) {
+  BinaryImageType::Pointer label = BinaryImageType::New();
+  if (!readImage<BinaryImageType>(label, labelFile)) {
     return EXIT_FAILURE;
   }
 
-  std::cout << "input image " << imageFile << std::endl;
-  std::cout << "   size " << image->GetLargestPossibleRegion().GetSize() << std::endl;
-  std::cout << "spacing " << image->GetSpacing() << std::endl;
-  std::cout << " origin " << image->GetOrigin() << std::endl;
+  std::cout << "input label info" << std::endl;
+  std::cout << labelFile << std::endl;
+  std::cout << "   size " << label->GetLargestPossibleRegion().GetSize() << std::endl;
+  std::cout << "spacing " << label->GetSpacing() << std::endl;
+  std::cout << " origin " << label->GetOrigin() << std::endl;
   std::cout << std::endl;
+
+  typedef itk::RecursiveGaussianImageFilter<BinaryImageType, FloatImageType> RecursiveGaussianImageFilterType;
+  RecursiveGaussianImageFilterType::Pointer gaussian = RecursiveGaussianImageFilterType::New();
+  gaussian->SetInput(label);
+  gaussian->SetSigma(sigma);
+  gaussian->Update();
+
+  typedef itk::GrayscaleFillholeImageFilter<FloatImageType, FloatImageType> GrayscaleFillholeImageFilterType;
+  GrayscaleFillholeImageFilterType::Pointer fillholes = GrayscaleFillholeImageFilterType::New();
+  fillholes->SetInput(gaussian->GetOutput());
+  fillholes->SetFullyConnected(true);
+  fillholes->Update();
 
   // resampling of input image
   BinaryImageType::SpacingType outputSpacing;
@@ -72,33 +92,23 @@ int main(int argc, char** argv) {
   BinaryImageType::SizeType outputSize;
 
   for (int i = 0; i < Dimension; ++i) {
-    outputSize[i] = image->GetLargestPossibleRegion().GetSize()[i] * image->GetSpacing()[i] / spacing;
+    outputSize[i] = label->GetLargestPossibleRegion().GetSize()[i] * label->GetSpacing()[i] / spacing;
   }
-  
-  typedef itk::RecursiveGaussianImageFilter<BinaryImageType, FloatImageType> RecursiveGaussianImageFilterType;
-  RecursiveGaussianImageFilterType::Pointer gaussian = RecursiveGaussianImageFilterType::New();
-  gaussian->SetSigma(sigma);
-  gaussian->SetInput(image);
-  gaussian->Update();
 
   typedef itk::ResampleImageFilter<FloatImageType, FloatImageType> ResampleImageFilterType;
   ResampleImageFilterType::Pointer resampler = ResampleImageFilterType::New();
   resampler->SetDefaultPixelValue(0);
   resampler->SetSize(outputSize);
   resampler->SetOutputSpacing(outputSpacing);
-  resampler->SetOutputOrigin(gaussian->GetOutput()->GetOrigin());
-  resampler->SetOutputDirection(gaussian->GetOutput()->GetDirection());
-  resampler->SetInput(gaussian->GetOutput());
+  resampler->SetOutputOrigin(fillholes->GetOutput()->GetOrigin());
+  resampler->SetOutputDirection(fillholes->GetOutput()->GetDirection());
+  resampler->SetInput(fillholes->GetOutput());
   resampler->Update();
 
-  typedef itk::GrayscaleFillholeImageFilter<FloatImageType, FloatImageType> GrayscaleFillholeImageFilterType;
-  GrayscaleFillholeImageFilterType::Pointer fillholes = GrayscaleFillholeImageFilterType::New();
-  fillholes->SetInput(resampler->GetOutput());
-  fillholes->SetFullyConnected(true);
-  fillholes->Update();
+  FloatImageType::Pointer processedImage = resampler->GetOutput();
 
-  FloatImageType::Pointer processedImage = fillholes->GetOutput();
-
+  //----------------------------------------------------------------------------
+  //compute surface
   typedef itk::MinimumMaximumImageCalculator <FloatImageType> MinimumMaximumImageCalculatorType;
   MinimumMaximumImageCalculatorType::Pointer calculator = MinimumMaximumImageCalculatorType::New();
   calculator->SetImage(processedImage);
@@ -127,8 +137,8 @@ int main(int argc, char** argv) {
   typedef vtkSmartPointer<vtkSmoothPolyDataFilter> SmoothPolyData;
   SmoothPolyData smoother = SmoothPolyData::New();
   smoother->SetInputData(mcubes->GetOutput());
-  smoother->SetNumberOfIterations(numberOfIterations);
-  smoother->SetRelaxationFactor(relaxationFactor);
+  smoother->SetNumberOfIterations(iterations);
+  smoother->SetRelaxationFactor(relaxation);
 
   try {
     smoother->Update();
@@ -155,17 +165,49 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
-  vtkSmartPointer<vtkPolyData> polyData = normals->GetOutput();
+  vtkSmartPointer<vtkPolyData> surface = normals->GetOutput();
 
   // write polydata to the file
-  if (!writeVTKPolydata(polyData, outputFile)) {
+  if (!writeVTKPolydata(surface, outputSurfaceFile)) {
+    return EXIT_FAILURE;
+  }
+  
+  std::cout << "output surface polydata info" << std::endl;
+  std::cout << outputSurfaceFile << std::endl;
+  std::cout << " number of cells " << surface->GetNumberOfCells() << std::endl;
+  std::cout << "number of points " << surface->GetNumberOfPoints() << std::endl;
+  std::cout << std::endl;
+
+  //----------------------------------------------------------------------------
+  //compute potential map image
+  typedef itk::AddImageFilter <FloatImageType, FloatImageType> AddImageFilterType;
+  AddImageFilterType::Pointer addImageFilter = AddImageFilterType::New();
+  addImageFilter->SetInput(processedImage);
+  addImageFilter->SetConstant(-levelValue);
+  addImageFilter->Update();
+
+  typedef itk::ZeroCrossingImageFilter<FloatImageType, BinaryImageType > ZeroCrossingImageFilterType;
+  ZeroCrossingImageFilterType::Pointer zerocross = ZeroCrossingImageFilterType::New();
+  zerocross->SetInput(addImageFilter->GetOutput());
+
+  typedef itk::SignedMaurerDistanceMapImageFilter<BinaryImageType, FloatImageType> SignedMaurerDistanceMapImageFilterType;
+  SignedMaurerDistanceMapImageFilterType::Pointer distancemap = SignedMaurerDistanceMapImageFilterType::New();
+  distancemap->SetUseImageSpacing(true);
+  distancemap->SetInput(zerocross->GetOutput());
+  distancemap->Update();
+
+  FloatImageType::Pointer potentialImage = distancemap->GetOutput();
+
+  // write potential image to the file
+  if (!writeImage<FloatImageType>(potentialImage, outputPotentialFile)) {
     return EXIT_FAILURE;
   }
 
-  std::cout << "output polydata " << outputFile << std::endl;
-  std::cout << " number of cells " << polyData->GetNumberOfCells() << std::endl;
-  std::cout << "number of points " << polyData->GetNumberOfPoints() << std::endl;
-  std::cout << std::endl;
+  std::cout << "output potential image info" << std::endl;
+  std::cout << outputPotentialFile << std::endl;
+  std::cout << "   size " << potentialImage->GetLargestPossibleRegion().GetSize() << std::endl;
+  std::cout << "spacing " << potentialImage->GetSpacing() << std::endl;
+  std::cout << " origin " << potentialImage->GetOrigin() << std::endl;
 
   return EXIT_SUCCESS;
 }
