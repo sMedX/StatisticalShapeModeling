2a8555edf5c48df2c20e0622cdf0eec0c8c90b77
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2016-06-05 15:29:27 +0300

    update application

--- ReferenceShapeConstructor.cxx
+++ ReferenceShapeConstructor.cxx
@@ -1,8 +1,5 @@
 #include <itkMesh.h>
 #include <itkImage.h>
-#include <itkBoundingBox.h>
-#include <itkAddImageFilter.h>
-#include <itkImageDuplicator.h>
 #include <utils/statismo-build-models-utils.h>
 
 #include "utils/io.h"
@@ -30,6 +27,13 @@ int main(int argc, char** argv) {
   int numberOfIterations = 100;
   parser->GetCommandLineArgument("-iteration", numberOfIterations);
 
+  std::cout << std::endl;
+  std::cout << "reference shape constructor" << std::endl;
+  std::cout << "   list of files " << listFile << std::endl;
+  std::cout << "     output file " << outputFile << std::endl;
+  std::cout << "      iterations " << numberOfIterations << std::endl;
+  std::cout << std::endl;
+
   //----------------------------------------------------------------------------
   // read list of files
   StringList fileNames = getFileList(listFile);
@@ -41,7 +45,7 @@ int main(int argc, char** argv) {
   }
 
   //----------------------------------------------------------------------------
-  // compute distance map image
+  // compute level set image
   typedef SurfaceToLevelSetImageFilter<MeshType, FloatImageType> SurfaceToPotentialImageFilterType;
   SurfaceToPotentialImageFilterType::Pointer levelset = SurfaceToPotentialImageFilterType::New();
   levelset->SetMargin(0.5);
@@ -57,7 +61,12 @@ int main(int argc, char** argv) {
 
   FloatImageType::Pointer reference = levelset->GetOutput();
 
-  //allocate image
+  // reference image data
+  FloatImageType::PointType origin = reference->GetOrigin();
+  FloatImageType::SpacingType spacing = reference->GetSpacing();
+  FloatImageType::SizeType size = reference->GetLargestPossibleRegion().GetSize();
+
+  // allocate image
   FloatImageType::Pointer updateReference = FloatImageType::New();
   updateReference->SetRegions(reference->GetLargestPossibleRegion());
   updateReference->Allocate();
@@ -66,6 +75,8 @@ int main(int argc, char** argv) {
 
   //----------------------------------------------------------------------------
   // compute reference image
+  double count = 0;
+
   for (StringList::const_iterator it = fileNames.begin(); it != fileNames.end(); ++it) {
     std::string fileName = it->c_str();
     MeshType::Pointer surface = MeshType::New();
@@ -78,39 +89,52 @@ int main(int argc, char** argv) {
     std::cout << " number of cells " << surface->GetNumberOfCells() << std::endl;
     std::cout << "number of points " << surface->GetNumberOfPoints() << std::endl;
 
-    //perform registration
+    // perform registration
     typedef SurfaceToImageRegistrationFilter<MeshType> SurfaceToImageRegistrationFilterType;
     SurfaceToImageRegistrationFilterType::Pointer surfaceToImageRegistration = SurfaceToImageRegistrationFilterType::New();
     surfaceToImageRegistration->SetNumberOfIterations(numberOfIterations);
     surfaceToImageRegistration->SetInput(surface);
     surfaceToImageRegistration->SetPotentialImage(reference);
-    surfaceToImageRegistration->Update();
-    surfaceToImageRegistration->PrintReport(std::cout);
-
-    //compute level set image
-    typedef SurfaceToLevelSetImageFilter<MeshType, FloatImageType> SurfaceToPotentialImageFilterType;
-    SurfaceToPotentialImageFilterType::Pointer levelset = SurfaceToPotentialImageFilterType::New();
-    levelset->SetInput(surfaceToImageRegistration->GetOutput());
-    levelset->SetOrigin(reference->GetOrigin());
-    levelset->SetSpacing(reference->GetSpacing());
-    levelset->SetSize(reference->GetLargestPossibleRegion().GetSize());
-    levelset->Update();
-
-    //add images
-    typedef itk::AddImageFilter <FloatImageType, FloatImageType> AddImageFilterType;
-    AddImageFilterType::Pointer addFilter = AddImageFilterType::New();
-    addFilter->SetInput1(updateReference);
-    addFilter->SetInput2(levelset->GetOutput());
 
     try {
-      addFilter->Update();
+      surfaceToImageRegistration->Update();
     }
     catch (itk::ExceptionObject& excep) {
       std::cerr << excep << std::endl;
       return EXIT_FAILURE;
     }
 
-    updateReference = addFilter->GetOutput();
+    surfaceToImageRegistration->PrintReport(std::cout);
+
+    // compute level set image
+    typedef SurfaceToLevelSetImageFilter<MeshType, FloatImageType> SurfaceToPotentialImageFilterType;
+    SurfaceToPotentialImageFilterType::Pointer levelset = SurfaceToPotentialImageFilterType::New();
+    levelset->SetOrigin(origin);
+    levelset->SetSpacing(spacing);
+    levelset->SetSize(size);
+    levelset->SetInput(surfaceToImageRegistration->GetOutput());
+
+    try {
+      levelset->Update();
+    }
+    catch (itk::ExceptionObject& excep) {
+      std::cerr << excep << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    // add current levelset to the reference image
+    count++;
+    double const1 = 1 - 1 / count;
+    double const2 = 1 / count;
+
+    typedef itk::ImageRegionIterator<FloatImageType> IteratorType;
+    IteratorType it1(updateReference, updateReference->GetLargestPossibleRegion());
+    IteratorType it2(levelset->GetOutput(), levelset->GetOutput()->GetLargestPossibleRegion());
+
+    for (it1.GoToBegin(), it2.GoToBegin(); !it1.IsAtEnd(); ++it1, ++it2) {
+      double pixelValue = const1 * it1.Get() + const2*it2.Get();
+      it1.Set(pixelValue);
+    }
   }
 
   /*
