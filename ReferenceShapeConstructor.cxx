f6ed663c1f59d5716b079dfb9f6216e0e6ecda88
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2016-06-06 14:27:45 +0300

    building of reference surface via two stage computations

--- ReferenceShapeConstructor.cxx
+++ ReferenceShapeConstructor.cxx
@@ -42,86 +42,39 @@ int main(int argc, char** argv) {
   std::cout << "      iterations " << numberOfIterations << std::endl;
   std::cout << std::endl;
 
-  //----------------------------------------------------------------------------
   // read list of files
   StringList fileNames = getFileList(listFile);
-
-  std::string fileName = fileNames.begin()->c_str();
-  MeshType::Pointer surface = MeshType::New();
-  if (!readMesh<MeshType>(surface, fileName)) {
-    return EXIT_FAILURE;
-  }
+  std::string refFileName = fileNames.begin()->c_str();
 
   //----------------------------------------------------------------------------
-  // compute level set image
+  // compute reference image 
+
+  // define types
   typedef SurfaceToLevelSetImageFilter<MeshType, FloatImageType> SurfaceToPotentialImageFilterType;
-  SurfaceToPotentialImageFilterType::Pointer levelset = SurfaceToPotentialImageFilterType::New();
-  levelset->SetMargin(0.5);
-  levelset->SetInput(surface);
+  typedef SurfaceToImageRegistrationFilter<MeshType> SurfaceToImageRegistrationFilterType;
+  typedef SurfaceToImageRegistrationFilterType::EnumTransformType EnumTransformType;
+  EnumTransformType typeOfTransform = EnumTransformType::Rotation;
 
-  try {
-    levelset->Update();
-  }
-  catch (itk::ExceptionObject& excep) {
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-  }
+  for (int stage = 0; stage < 2; ++stage) {
+    if (stage > 0) {
+      typeOfTransform = EnumTransformType::Affine;
+      refFileName = outputSurface;
+    }
+    std::cout << "stage " << stage << std::endl;
+    std::cout << "type of transform " << typeOfTransform << std::endl;
+    std::cout << "reference surface " << refFileName << std::endl;
 
-  FloatImageType::Pointer reference = levelset->GetOutput();
-
-  // reference image data
-  FloatImageType::PointType origin = reference->GetOrigin();
-  FloatImageType::SpacingType spacing = reference->GetSpacing();
-  FloatImageType::SizeType size = reference->GetLargestPossibleRegion().GetSize();
-
-  // allocate image
-  FloatImageType::Pointer updateReference = FloatImageType::New();
-  updateReference->SetRegions(reference->GetLargestPossibleRegion());
-  updateReference->Allocate();
-  updateReference->FillBuffer(0);
-  updateReference->CopyInformation(reference);
-
-  //----------------------------------------------------------------------------
-  // compute reference image
-  double count = 0;
-
-  for (StringList::const_iterator it = fileNames.begin(); it != fileNames.end(); ++it) {
-    std::string fileName = it->c_str();
+    // read reference surface
     MeshType::Pointer surface = MeshType::New();
-    if (!readMesh<MeshType>(surface, fileName)) {
+    if (!readMesh<MeshType>(surface, refFileName)) {
       return EXIT_FAILURE;
     }
-
-    std::cout << std::endl;
-    std::cout << "surface polydata info " << fileName << std::endl;
-    std::cout << " number of cells " << surface->GetNumberOfCells() << std::endl;
-    std::cout << "number of points " << surface->GetNumberOfPoints() << std::endl;
-
-    // perform registration
-    typedef SurfaceToImageRegistrationFilter<MeshType> SurfaceToImageRegistrationFilterType;
-    SurfaceToImageRegistrationFilterType::Pointer surfaceToImageRegistration = SurfaceToImageRegistrationFilterType::New();
-    surfaceToImageRegistration->SetNumberOfIterations(numberOfIterations);
-    surfaceToImageRegistration->SetInput(surface);
-    surfaceToImageRegistration->SetTransformType(SurfaceToImageRegistrationFilterType::EnumTransformType::Rotation);
-    surfaceToImageRegistration->SetLevelsetImage(reference);
-
-    try {
-      surfaceToImageRegistration->Update();
-    }
-    catch (itk::ExceptionObject& excep) {
-      std::cerr << excep << std::endl;
-      return EXIT_FAILURE;
-    }
-
-    surfaceToImageRegistration->PrintReport(std::cout);
 
     // compute level set image
     typedef SurfaceToLevelSetImageFilter<MeshType, FloatImageType> SurfaceToPotentialImageFilterType;
     SurfaceToPotentialImageFilterType::Pointer levelset = SurfaceToPotentialImageFilterType::New();
-    levelset->SetOrigin(origin);
-    levelset->SetSpacing(spacing);
-    levelset->SetSize(size);
-    levelset->SetInput(surfaceToImageRegistration->GetOutput());
+    levelset->SetMargin(0.5);
+    levelset->SetInput(surface);
 
     try {
       levelset->Update();
@@ -131,95 +84,151 @@ int main(int argc, char** argv) {
       return EXIT_FAILURE;
     }
 
-    // add current levelset to the reference image
-    count++;
-    double const1 = 1 - 1 / count;
-    double const2 = 1 / count;
+    FloatImageType::Pointer reference = levelset->GetOutput();
 
-    typedef itk::ImageRegionIterator<FloatImageType> IteratorType;
-    IteratorType it1(updateReference, updateReference->GetLargestPossibleRegion());
-    IteratorType it2(levelset->GetOutput(), levelset->GetOutput()->GetLargestPossibleRegion());
+    // allocate image to update reference image
+    FloatImageType::Pointer updateReference = FloatImageType::New();
+    updateReference->SetRegions(reference->GetLargestPossibleRegion());
+    updateReference->Allocate();
+    updateReference->FillBuffer(0);
+    updateReference->CopyInformation(reference);
 
-    for (it1.GoToBegin(), it2.GoToBegin(); !it1.IsAtEnd(); ++it1, ++it2) {
-      double pixelValue = const1 * it1.Get() + const2*it2.Get();
-      it1.Set(pixelValue);
+    // compute reference image
+    double count = 0;
+
+    for (StringList::const_iterator it = fileNames.begin(); it != fileNames.end(); ++it) {
+      std::string fileName = it->c_str();
+      MeshType::Pointer surface = MeshType::New();
+      if (!readMesh<MeshType>(surface, fileName)) {
+        return EXIT_FAILURE;
+      }
+
+      std::cout << std::endl;
+      std::cout << "surface polydata info " << fileName << std::endl;
+      std::cout << " number of cells " << surface->GetNumberOfCells() << std::endl;
+      std::cout << "number of points " << surface->GetNumberOfPoints() << std::endl;
+
+      // perform registration
+      SurfaceToImageRegistrationFilterType::Pointer surfaceToImageRegistration = SurfaceToImageRegistrationFilterType::New();
+      surfaceToImageRegistration->SetNumberOfIterations(numberOfIterations);
+      surfaceToImageRegistration->SetInput(surface);
+
+      surfaceToImageRegistration->SetTransformType(typeOfTransform);
+      surfaceToImageRegistration->SetLevelsetImage(reference);
+
+      try {
+        surfaceToImageRegistration->Update();
+      }
+      catch (itk::ExceptionObject& excep) {
+        std::cerr << excep << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      surfaceToImageRegistration->PrintReport(std::cout);
+
+      // compute level set image
+      SurfaceToPotentialImageFilterType::Pointer levelset = SurfaceToPotentialImageFilterType::New();
+      levelset->SetOrigin(reference->GetOrigin());
+      levelset->SetSpacing(reference->GetSpacing());
+      levelset->SetSize(reference->GetLargestPossibleRegion().GetSize());
+      levelset->SetInput(surfaceToImageRegistration->GetOutput());
+
+      try {
+        levelset->Update();
+      }
+      catch (itk::ExceptionObject& excep) {
+        std::cerr << excep << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      // add current levelset to the reference image
+      count++;
+      double const1 = 1 - 1 / count;
+      double const2 = 1 / count;
+
+      typedef itk::ImageRegionIterator<FloatImageType> IteratorType;
+      IteratorType it1(updateReference, updateReference->GetLargestPossibleRegion());
+      IteratorType it2(levelset->GetOutput(), levelset->GetOutput()->GetLargestPossibleRegion());
+
+      for (it1.GoToBegin(), it2.GoToBegin(); !it1.IsAtEnd(); ++it1, ++it2) {
+        double pixelValue = const1 * it1.Get() + const2*it2.Get();
+        it1.Set(pixelValue);
+      }
     }
-  }
 
-  std::cout << "output reference image " << outputLevelset << std::endl;
-  std::cout << "   size " << updateReference->GetLargestPossibleRegion().GetSize() << std::endl;
-  std::cout << "spacing " << updateReference->GetSpacing() << std::endl;
-  std::cout << " origin " << updateReference->GetOrigin() << std::endl;
-  std::cout << std::endl;
+    std::cout << "output reference image " << outputLevelset << std::endl;
+    std::cout << "   size " << updateReference->GetLargestPossibleRegion().GetSize() << std::endl;
+    std::cout << "spacing " << updateReference->GetSpacing() << std::endl;
+    std::cout << " origin " << updateReference->GetOrigin() << std::endl;
+    std::cout << std::endl;
 
-  if (!writeImage<FloatImageType>(updateReference, outputLevelset)) {
-    return EXIT_FAILURE;
-  }
+    if (!writeImage<FloatImageType>(updateReference, outputLevelset)) {
+      return EXIT_FAILURE;
+    }
 
-  //----------------------------------------------------------------------------
-  // compute reference surface
+    // compute reference surface, convert ITK image to VTK image
+    typedef itk::ImageToVTKImageFilter<FloatImageType> ConvertorType;
+    ConvertorType::Pointer convertor = ConvertorType::New();
+    convertor->SetInput(updateReference);
+    convertor->Update();
 
-  //convert ITK image to VTK image
-  typedef itk::ImageToVTKImageFilter<FloatImageType> ConvertorType;
-  ConvertorType::Pointer convertor = ConvertorType::New();
-  convertor->SetInput(updateReference);
-  convertor->Update();
+    double levelValue = 0;
+    typedef vtkSmartPointer<vtkMarchingCubes> MarchingCubes;
+    MarchingCubes mcubes = MarchingCubes::New();
+    mcubes->SetInputData(convertor->GetOutput());
+    mcubes->SetValue(0, levelValue);
 
-  double levelValue = 0;
-  typedef vtkSmartPointer<vtkMarchingCubes> MarchingCubes;
-  MarchingCubes mcubes = MarchingCubes::New();
-  mcubes->SetInputData(convertor->GetOutput());
-  mcubes->SetValue(0, levelValue);
+    try {
+      mcubes->Update();
+    }
+    catch (itk::ExceptionObject& excep) {
+      std::cerr << excep << std::endl;
+      return EXIT_FAILURE;
+    }
 
-  try {
-    mcubes->Update();
-  }
-  catch (itk::ExceptionObject& excep) {
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-  }
-  float relaxation = 0.2;
-  int iterations = 10;
+    float relaxation = 0.2;
+    int iterations = 10;
 
-  typedef vtkSmartPointer<vtkSmoothPolyDataFilter> SmoothPolyData;
-  SmoothPolyData smoother = SmoothPolyData::New();
-  smoother->SetInputData(mcubes->GetOutput());
-  smoother->SetNumberOfIterations(iterations);
-  smoother->SetRelaxationFactor(relaxation);
+    typedef vtkSmartPointer<vtkSmoothPolyDataFilter> SmoothPolyData;
+    SmoothPolyData smoother = SmoothPolyData::New();
+    smoother->SetInputData(mcubes->GetOutput());
+    smoother->SetNumberOfIterations(iterations);
+    smoother->SetRelaxationFactor(relaxation);
 
-  try {
-    smoother->Update();
-  }
-  catch (itk::ExceptionObject& excep) {
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-  }
+    try {
+      smoother->Update();
+    }
+    catch (itk::ExceptionObject& excep) {
+      std::cerr << excep << std::endl;
+      return EXIT_FAILURE;
+    }
 
-  typedef vtkSmartPointer<vtkPolyDataNormals> PolyDataNormals;
-  PolyDataNormals normals = PolyDataNormals::New();
-  normals->SetInputData(smoother->GetOutput());
-  normals->AutoOrientNormalsOn();
-  normals->FlipNormalsOff();
-  normals->ConsistencyOn();
-  normals->ComputeCellNormalsOff();
-  normals->SplittingOff();
+    typedef vtkSmartPointer<vtkPolyDataNormals> PolyDataNormals;
+    PolyDataNormals normals = PolyDataNormals::New();
+    normals->SetInputData(smoother->GetOutput());
+    normals->AutoOrientNormalsOn();
+    normals->FlipNormalsOff();
+    normals->ConsistencyOn();
+    normals->ComputeCellNormalsOff();
+    normals->SplittingOff();
 
-  try {
-    normals->Update();
-  }
-  catch (itk::ExceptionObject& excep) {
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-  }
+    try {
+      normals->Update();
+    }
+    catch (itk::ExceptionObject& excep) {
+      std::cerr << excep << std::endl;
+      return EXIT_FAILURE;
+    }
 
-  // write surface to the file
-  std::cout << "output reference surface " << outputSurface << std::endl;
-  std::cout << " number of cells " << normals->GetOutput()->GetNumberOfCells() << std::endl;
-  std::cout << "number of points " << normals->GetOutput()->GetNumberOfPoints() << std::endl;
-  std::cout << std::endl;
+    // write surface to the file
+    std::cout << "output reference surface " << outputSurface << std::endl;
+    std::cout << " number of cells " << normals->GetOutput()->GetNumberOfCells() << std::endl;
+    std::cout << "number of points " << normals->GetOutput()->GetNumberOfPoints() << std::endl;
+    std::cout << std::endl;
 
-  if (!writeVTKPolydata(normals->GetOutput(), outputSurface)) {
-    return EXIT_FAILURE;
+    if (!writeVTKPolydata(normals->GetOutput(), outputSurface)) {
+      return EXIT_FAILURE;
+    }
   }
 
   return EXIT_SUCCESS;
