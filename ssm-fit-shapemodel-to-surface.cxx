ec7a814f0e364cdb1a609d183259cbe66fef172f
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2017-02-03 15:55:59 +0300

    update cli for ssm-fit-shapemodel-to-surface

--- ssm-fit-shapemodel-to-surface.cxx
+++ ssm-fit-shapemodel-to-surface.cxx
@@ -1,4 +1,6 @@
 ﻿#include <boost/program_options.hpp>
+
+#include <boost/program_options.hpp>
 #include <itkImageMomentsCalculator.h>
 #include <itkTriangleMeshToBinaryImageFilter.h>
 #include <itkSignedMaurerDistanceMapImageFilter.h>
@@ -8,14 +10,50 @@
 
 #include "utils/ssmTypes.h"
 #include "utils/io.h"
-#include "utils/itkCommandLineArgumentParser.h"
-
 #include "ssm/ssmPointSetToImageMetrics.h"
 #include "ssm/ssmShapeModelToImageRegistrationMethod.h"
 #include "ssm/ssmSurfaceToLevelSetImageFilter.h"
 
+struct ProgramOptions
+{
+  bool help;
+  std::string modelFile;
+  std::string surfaceFile;
+  std::string outputFile;
+  std::string levelsetFile;
+  std::string reportFile;
+  std::string transformFile;
+  size_t transform = 1;
+  size_t iterations = 500;
+  size_t degree = 2;
+  double regularization = 0.1;
+};
+
+namespace po = boost::program_options;
+po::options_description initializeProgramOptions(ProgramOptions& poParameters);
+
 int main(int argc, char** argv)
 {
+  ProgramOptions options;
+  po::options_description description = initializeProgramOptions(options);
+  po::variables_map vm;
+  try {
+    po::parsed_options parsedOptions = po::command_line_parser(argc, argv).options(description).run();
+    po::store(parsedOptions, vm);
+    po::notify(vm);
+  }
+  catch (po::error& e) {
+    cerr << "An exception occurred while parsing the command line:" << endl;
+    cerr << e.what() << endl << endl;
+    cout << description << endl;
+    return EXIT_FAILURE;
+  }
+  if (options.help == true) {
+    cout << description << endl;
+    return EXIT_SUCCESS;
+  }
+
+  /*
   itk::CommandLineArgumentParser::Pointer parser = itk::CommandLineArgumentParser::New();
 
   parser->SetCommandLineArguments(argc, argv);
@@ -51,11 +89,11 @@ int main(int argc, char** argv)
   std::cout << "                degree " << degree << std::endl;
   std::cout << "        regularization " << regularization << std::endl;
   std::cout << std::endl;
-
+  */
   //----------------------------------------------------------------------------
   // read surface
   MeshType::Pointer surface = MeshType::New();
-  if (!readMesh<MeshType>(surface, surfaceFile)) {
+  if (!readMesh<MeshType>(surface, options.surfaceFile)) {
     return EXIT_FAILURE;
   }
   std::cout << "surface " << surface << std::endl;
@@ -71,14 +109,14 @@ int main(int argc, char** argv)
   typedef itk::StatisticalModel<MeshType> StatisticalModelType;
   StatisticalModelType::Pointer model = StatisticalModelType::New();
   try {
-    model->Load(representer, modelFile.c_str());
+    model->Load(representer, options.modelFile.c_str());
   }
   catch (itk::ExceptionObject & excp) {
     std::cerr << excp << std::endl;
     return EXIT_FAILURE;
   }
 
-  std::cout << "model " << modelFile << std::endl;
+  std::cout << "model " << options.modelFile << std::endl;
   std::cout << "number of components " << model->GetNumberOfPrincipalComponents() << std::endl;
   std::cout << "number of points     " << model->GetRepresenter()->GetReference()->GetNumberOfPoints() << std::endl;
   std::cout << std::endl;
@@ -141,7 +179,7 @@ int main(int argc, char** argv)
 
   typedef ssm::TransformInitializer<double> TransformInitializerType;
   TransformInitializerType::Pointer initializer = TransformInitializerType::New();
-  initializer->SetTypeOfTransform(typeOfransform);
+  initializer->SetTypeOfTransform(options.transform);
   initializer->SetCenter(center);
   initializer->SetTranslation(translation);
   try {
@@ -162,9 +200,9 @@ int main(int argc, char** argv)
   shapeModelToSurfaceRegistration = ShapeModelRegistrationMethod::New();
   shapeModelToSurfaceRegistration->SetShapeModel(model);
   shapeModelToSurfaceRegistration->SetLevelSetImage(levelset->GetOutput());
-  shapeModelToSurfaceRegistration->SetNumberOfIterations(numberOfIterations);
-  shapeModelToSurfaceRegistration->SetRegularizationParameter(regularization);
-  shapeModelToSurfaceRegistration->SetDegree(degree);
+  shapeModelToSurfaceRegistration->SetNumberOfIterations(options.iterations);
+  shapeModelToSurfaceRegistration->SetRegularizationParameter(options.regularization);
+  shapeModelToSurfaceRegistration->SetDegree(options.degree);
   shapeModelToSurfaceRegistration->SetTransformInitializer(initializer);
   try {
     shapeModelToSurfaceRegistration->Update();
@@ -195,26 +233,66 @@ int main(int argc, char** argv)
   metrics->PrintReport(std::cout);
 
   // write report to *.csv file
-  if (parser->ArgumentExists("-report")) {
-    std::string fileName;
-    parser->GetCommandLineArgument("-report", fileName);
-    std::cout << "print report to the file: " << fileName << std::endl;
-    metrics->PrintReportToFile(fileName, getBaseNameFromPath(surfaceFile));
+  if (options.reportFile != "") {
+    metrics->PrintReportToFile(options.reportFile, getBaseNameFromPath(options.surfaceFile));
   }
 
   // write surface
-  if (!writeMesh<MeshType>(shapeModelToSurfaceRegistration->GetOutput(), outputFile)) {
+  if (!writeMesh<MeshType>(shapeModelToSurfaceRegistration->GetOutput(), options.outputFile)) {
     return EXIT_FAILURE;
   }
 
   // write levelset image
-  if (parser->ArgumentExists("-levelset")) {
-    std::string fileName;
-    parser->GetCommandLineArgument("-levelset", fileName);
-    if (!writeImage(shapeModelToSurfaceRegistration->GetLevelSetImage(), fileName)) {
+  if (options.levelsetFile != "" ) {
+    if (!writeImage(shapeModelToSurfaceRegistration->GetLevelSetImage(), options.reportFile)) {
       return EXIT_FAILURE;
     }
   }
 
+  // write transform
+  if (options.transformFile != "") {
+    std::cout << "write transform to the file: " << options.transformFile << std::endl;
+    writeTransform(initializer->GetTransform(), options.transformFile);
+  }
+
   return EXIT_SUCCESS;
+}
+
+po::options_description initializeProgramOptions(ProgramOptions& options)
+{
+  po::options_description mandatory("Mandatory options");
+  mandatory.add_options()
+    ("model,m", po::value<std::string>(&options.modelFile), "The path to input shape model file.")
+    ("surface,i", po::value<std::string>(&options.surfaceFile), "The path to the input surface file.")
+    ("output,o", po::value<std::string>(&options.outputFile), "The path for the output surface file.")
+    ;
+
+  po::options_description input("Optional input options");
+  input.add_options()
+    ("transform", po::value<size_t>(&options.transform)->default_value(options.transform), "The sigma of the Gaussian kernel measured in world coordinates.")
+    ("iterations", po::value<size_t>(&options.iterations)->default_value(options.iterations), "The relaxation factor for Laplacian smoothing.")
+    ("degree", po::value<size_t>(&options.degree)->default_value(options.degree), "The relaxation factor for Laplacian smoothing.")
+    ("regularization", po::value<double>(&options.regularization)->default_value(options.regularization), "The number of iterations.")
+    ;
+
+  po::options_description output("Optional output options");
+  output.add_options()
+    ("output-levelset", po::value<std::string>(&options.levelsetFile), "The path for the output level-set image.")
+    ("output-transform", po::value<std::string>(&options.transformFile), "The path for the output transform file.")
+    ;
+
+  po::options_description report("Optional report options");
+  report.add_options()
+    ("report,r", po::value<std::string>(&options.reportFile), "The path for the file to print report.")
+    ;
+
+  po::options_description help("Optional options");
+  help.add_options()
+    ("help,h", po::bool_switch(&options.help), "Display this help message")
+    ;
+
+  po::options_description description;
+  description.add(mandatory).add(input).add(output).add(report).add(help);
+
+  return description;
 }
