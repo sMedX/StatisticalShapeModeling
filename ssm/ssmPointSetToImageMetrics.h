41d9f01983c9cc3d08983539e95de1a2b6aa0334
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2017-02-09 13:16:04 +0300

    update metrics

--- ssm/ssmPointSetToImageMetrics.h
+++ ssm/ssmPointSetToImageMetrics.h
@@ -1,22 +1,25 @@
 #pragma once
+#include <boost/filesystem.hpp>
 
-#include <itkPointSetToImageMetric.h>
-#include <itkCovariantVector.h>
-#include <itkPoint.h>
+#include <itkObject.h>
+#include <itkPointSet.h>
 #include <itkLinearInterpolateImageFunction.h>
+#include <itkSampleToHistogramFilter.h>
+#include <itkListSample.h>
+#include <itkHistogram.h>
 
 namespace ssm
 {
   template< typename TFixedPointSet, typename TMovingImage >
-  class PointSetToImageMetrics : public itk::PointSetToImageMetric < TFixedPointSet, TMovingImage >
+  class PointSetToImageMetrics : public itk::Object
   {
   public:
 
     /** Standard class typedefs. */
     typedef PointSetToImageMetrics                      Self;
-    typedef itk::PointSetToImageMetric< TFixedPointSet, TMovingImage > Superclass;
-    typedef itk::SmartPointer< Self >                                  Pointer;
-    typedef itk::SmartPointer< const Self >                            ConstPointer;
+    typedef itk::Object                                 Superclass;
+    typedef itk::SmartPointer< Self >                   Pointer;
+    typedef itk::SmartPointer< const Self >             ConstPointer;
 
     /** Method for creation through the object factory. */
     itkNewMacro(Self);
@@ -25,43 +28,35 @@ namespace ssm
     itkTypeMacro(PointSetToImageMetrics, Object);
 
     /** Types transferred from the base class */
-    typedef typename Superclass::RealType                RealType;
-    typedef typename Superclass::TransformType           TransformType;
-    typedef typename Superclass::TransformPointer        TransformPointer;
-    typedef typename Superclass::TransformParametersType TransformParametersType;
-    typedef typename Superclass::TransformJacobianType   TransformJacobianType;
-    typedef typename Superclass::GradientPixelType       GradientPixelType;
-    typedef typename Superclass::InputPointType          InputPointType;
-    typedef typename Superclass::OutputPointType         OutputPointType;
+    typedef typename double                              MeasureType;
+    typedef typename TFixedPointSet                      FixedPointSetType;
+    typedef typename FixedPointSetType::PointType        FixedPointType;
+    typedef typename FixedPointSetType::ConstPointer     FixedPointSetConstPointer;
+    typedef typename TMovingImage                        MovingImageType;
+    typedef typename MovingImageType::ConstPointer       MovingImageConstPointer;
 
-    typedef typename Superclass::MeasureType               MeasureType;
-    typedef typename Superclass::DerivativeType            DerivativeType;
-    typedef typename Superclass::FixedPointSetType         FixedPointSetType;
-    typedef typename Superclass::MovingImageType           MovingImageType;
-    typedef typename Superclass::FixedPointSetConstPointer FixedPointSetConstPointer;
-    typedef typename Superclass::MovingImageConstPointer   MovingImageConstPointer;
+    typedef typename FixedPointSetType::PointsContainerConstIterator     PointIterator;
+    typedef itk::LinearInterpolateImageFunction<TMovingImage, double>    InterpolatorType;
 
-    typedef typename Superclass::PointIterator     PointIterator;
-    typedef typename Superclass::PointDataIterator PointDataIterator;
-    typedef itk::LinearInterpolateImageFunction<TMovingImage, double> InterpolatorType;
-
-    /**  Type of the parameters. */
-    typedef typename Superclass::ParametersType ParametersType;
-
+    /**  Type of the additional information. */
     typedef std::pair<std::string, std::string> PairType;
     typedef std::vector<PairType> InfoType;
+    itkSetMacro(Info, InfoType);
 
-    /**  Compute values. */
-    void Compute();
+    /** Get/Set the fixed point set.  */
+    itkSetConstObjectMacro(FixedPointSet, FixedPointSetType);
+    itkGetConstObjectMacro(FixedPointSet, FixedPointSetType);
+
+    /** Get/Set the moving image.  */
+    itkSetConstObjectMacro(MovingImage, MovingImageType);
+    itkGetConstObjectMacro(MovingImage, MovingImageType);
 
     /*Get/Set values to compute quantile. */
     itkSetMacro(LevelOfQuantile, double);
     itkGetMacro(LevelOfQuantile, double);
 
-    itkSetMacro(HistogramSize, unsigned int);
-    itkGetMacro(HistogramSize, unsigned int);
-
-    itkSetMacro(Info, InfoType);
+    itkSetMacro(HistogramSize, size_t);
+    itkGetMacro(HistogramSize, size_t);
 
     /*Get metrics values. */
     itkGetMacro(MeanValue, MeasureType);
@@ -69,45 +64,139 @@ namespace ssm
     itkGetMacro(QuantileValue, MeasureType);
     itkGetMacro(MaximalValue, MeasureType);
 
-    void PrintReport(std::ostream& os) const;
-    void PrintReportToFile(const std::string & fileName, const std::string & datasetURI) const;
-
-    virtual MeasureType GetValue(const ParametersType& p) const
+    void PrintReport(std::ostream& os) const
     {
-      itkExceptionMacro(<< "Not implemented");
+      std::string indent = "    ";
+
+      os << "Metric values:" << std::endl;
+      os << indent << "    Mean = " << m_MeanValue << std::endl;
+      os << indent << "    RMSE = " << m_RMSEValue << std::endl;
+      os << indent << "Quantile = " << m_QuantileValue << ", level = " << m_LevelOfQuantile << std::endl;
+      os << indent << " Maximal = " << m_MaximalValue << std::endl;
+      os << std::endl;
     }
 
-    virtual void GetDerivative(const ParametersType& p, DerivativeType& dp) const
+    void PrintReportToFile(const std::string & fileName, const std::string & datasetURI) const
     {
-      itkExceptionMacro(<< "Not implemented");
+      std::string dlm = ";";
+
+      std::string header = dlm;
+      std::string scores = datasetURI + dlm;
+
+      header += "Mean" + dlm;
+      scores += std::to_string(m_MeanValue) + dlm;
+
+      header += "RMSE" + dlm;
+      scores += std::to_string(m_RMSEValue) + dlm;
+
+      header += "Quantile " + std::to_string(m_LevelOfQuantile) + dlm;
+      scores += std::to_string(m_QuantileValue) + dlm;
+
+      header += "Maximal" + dlm;
+      scores += std::to_string(m_MaximalValue) + dlm;
+
+      header += dlm;
+      scores += dlm;
+
+      for (auto it = m_Info.begin(); it != m_Info.end(); ++it) {
+        header += (*it).first + dlm;
+        scores += (*it).second + dlm;
+      }
+
+      bool exist = boost::filesystem::exists(fileName);
+      std::ofstream file(fileName, std::ofstream::out | std::ofstream::app);
+
+      if (!exist) {
+        file << header << std::endl;
+      }
+
+      file << scores << std::endl;
+      file.close();
     }
 
-    virtual void GetValueAndDerivative(const ParametersType& p, MeasureType& v, DerivativeType& dp) const
+    /**  Compute values. */
+    void Compute()
     {
-      itkExceptionMacro(<< "Not implemented");
+      if (!m_FixedPointSet) {
+        itkExceptionMacro(<< "Fixed point set has not been assigned");
+      }
+
+      if (!m_MovingImage) {
+        itkExceptionMacro(<< "Moving image has not been assigned");
+      }
+      m_Interpolator = InterpolatorType::New();
+      m_Interpolator->SetInputImage(m_MovingImage);
+
+      m_MeanValue = itk::NumericTraits<MeasureType>::Zero;
+      m_RMSEValue = itk::NumericTraits<MeasureType>::Zero;
+      m_MaximalValue = itk::NumericTraits<MeasureType>::Zero;
+
+      typedef itk::Vector<MeasureType, 1> VectorType;
+      typedef itk::Statistics::ListSample<VectorType> ListSampleType;
+      ListSampleType::Pointer sample = ListSampleType::New();
+
+      m_NumberOfPixelsCounted = 0;
+
+      for (PointIterator it = m_FixedPointSet->GetPoints()->Begin(); it != m_FixedPointSet->GetPoints()->End(); ++it) {
+        FixedPointType point = it.Value();
+
+        if (m_Interpolator->IsInsideBuffer(point)) {
+          const MeasureType value = std::abs(m_Interpolator->Evaluate(point));
+          sample->PushBack(value);
+
+          m_MeanValue += value;
+          m_RMSEValue += value * value;
+          m_MaximalValue = std::max(value, m_MaximalValue);
+
+          m_NumberOfPixelsCounted++;
+        }
+      }
+
+      if (!m_NumberOfPixelsCounted) {
+        itkExceptionMacro(<< "All the points mapped to outside of the moving image");
+      }
+
+      typedef typename itk::Statistics::Histogram<MeasureType, itk::Statistics::DenseFrequencyContainer2> HistogramType;
+      typename HistogramType::SizeType size(1);
+      size.Fill(m_HistogramSize);
+
+      typedef itk::Statistics::SampleToHistogramFilter<ListSampleType, HistogramType> SampleToHistogramFilterType;
+      SampleToHistogramFilterType::Pointer sampleToHistogram = SampleToHistogramFilterType::New();
+      sampleToHistogram->SetInput(sample);
+      sampleToHistogram->SetAutoMinimumMaximum(true);
+      sampleToHistogram->SetHistogramSize(size);
+      try {
+        sampleToHistogram->Update();
+      }
+      catch (itk::ExceptionObject& excep) {
+        itkExceptionMacro(<< excep);
+      }
+
+      m_MeanValue = m_MeanValue / m_NumberOfPixelsCounted;
+      m_RMSEValue = std::sqrt(m_RMSEValue / m_NumberOfPixelsCounted);
+      m_QuantileValue = sampleToHistogram->GetOutput()->Quantile(0, m_LevelOfQuantile);
     }
 
   protected:
-    PointSetToImageMetrics();
+    PointSetToImageMetrics() {}
     virtual ~PointSetToImageMetrics() {}
 
   private:
-    PointSetToImageMetrics(const Self &); //purposely not implemented
-    void operator=(const Self &);                   //purposely not implemented
+    PointSetToImageMetrics(const Self &);  // purposely not implemented
+    void operator=(const Self &);          // purposely not implemented
 
     typename InterpolatorType::Pointer m_Interpolator;
 
-    double m_LevelOfQuantile;
-    unsigned int m_HistogramSize;
-    RealType m_FixedValue;
-    MeasureType m_MeanValue = itk::NumericTraits<MeasureType>::Zero;
-    MeasureType m_RMSEValue = itk::NumericTraits<MeasureType>::Zero;
-    MeasureType m_QuantileValue = itk::NumericTraits<MeasureType>::Zero;
-    MeasureType m_MaximalValue = itk::NumericTraits<MeasureType>::Zero;
+    double m_LevelOfQuantile = 0.95;
+    size_t m_HistogramSize = 1000;
+    MeasureType m_MeanValue;
+    MeasureType m_RMSEValue;
+    MeasureType m_QuantileValue;
+    MeasureType m_MaximalValue;
     InfoType m_Info;
+    size_t m_NumberOfPixelsCounted;
+
+    typename FixedPointSetType::ConstPointer m_FixedPointSet;
+    typename MovingImageType::ConstPointer m_MovingImage;
   };
 }
-
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "ssmPointSetToImageMetrics.hxx"
-#endif
